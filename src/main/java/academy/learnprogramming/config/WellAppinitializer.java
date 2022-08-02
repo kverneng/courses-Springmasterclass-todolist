1c869ba8583df905cdac28787a2d86f85ee9d6d6
Kverneng <robertkverneng@hotmail.com>
2022-08-02 20:17:46 +0200

    Setup the WellApp for loading the servlet on startup

---
+++ src/main/java/academy/learnprogramming/config/WellAppinitializer.java
@@ -0,0 +1,33 @@
+package academy.learnprogramming.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+@Slf4j
+public class WellAppinitializer implements WebApplicationInitializer {
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        log.info("onStatup");
+
+        AnnotationConfigWebApplicationContext context
+                = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+
+        DispatcherServlet dispatcherServlet =
+                new DispatcherServlet(context);
+
+        ServletRegistration.Dynamic registration =
+                servletContext.addServlet(DISPATCHER_SERVLET_NAME,dispatcherServlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
