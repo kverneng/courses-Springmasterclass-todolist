e545f82cc7d9c90f72b32bf692de74b015d5eebb
Kverneng <robertkverneng@hotmail.com>
2022-08-03 12:08:53 +0200

    Adding a welcome jst view, and confiruing the vireResolver with prefix and suffix

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -14,4 +14,10 @@ public class DemoController {
         return "hello";
     }
 
+    // http://localhost:8080/todo-list/welcome
+    @GetMapping("welcome")
+    public String welcome(){
+        return "welcome";
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+<h1>Welcome World</h1>
+</body>
+</html>
