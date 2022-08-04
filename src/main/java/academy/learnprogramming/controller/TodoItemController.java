959fd7db81fb9abbf5fe195bab3c0a51b224d445
Kverneng <robertkverneng@hotmail.com>
2022-08-04 12:50:45 +0200

    Added redirect to view list from add_item

--- src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -1,13 +1,16 @@
 package academy.learnprogramming.controller;
 
 import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
 import academy.learnprogramming.service.TodoItemService;
+import academy.learnprogramming.util.AttributeNames;
 import academy.learnprogramming.util.Mappings;
 import academy.learnprogramming.util.ViewNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoItemController {
@@ -30,5 +33,10 @@ public class TodoItemController {
         return ViewNames.ITEMS_LIST;
     }
 
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem){
+
+        return "redirect:/" + Mappings.ITEMS;
+    }
 
 }
--- src/main/webapp/WEB-INF/view/add_item.jsp
+++ src/main/webapp/WEB-INF/view/add_item.jsp
@@ -1,12 +1,14 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="form" uri="http://springfremwork.org/tags/form" %>
+<%@ page import="academy.learnprogramming.util.AttributeNames" %>
+
 <html>
 <head>
     <title>Add Items</title>
 </head>
 <body>
     <div align="center">
-        <form:form method="POST">
+        <form:form method="POST" modelAttribute="${AttributeNames.TODO_ITEM}">
 
 
 
